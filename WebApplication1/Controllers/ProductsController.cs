8b4f7ea1f3bc88b3ca7abb08c57d19d4783f6213
Jad alKhawand <jkh42005@gmail.com>
2024-02-23 11:53:28 +0200

    CRUD operation is done

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using backend.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // CRUD => Crate - Read - Update - Delete
+
+        // Create
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct()
+        {
+            return null;
+        }
+
+        // Read
+
+        // Update
+
+        // Delete
+    }
+}
