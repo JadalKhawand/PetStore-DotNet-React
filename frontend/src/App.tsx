9370456fa44b40d04fdd2f37a8e658376c88c0eb
Jad alKhawand <jkh42005@gmail.com>
2024-02-24 15:26:08 +0200

    products page is done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "../src/components/pages/home/Home.page";
+import Product from "../src/components/pages/products/Products.page";
 const App: React.FC = () => {
   return (
     <div>
-      {/* Navbar */}
       <Navbar />
 
-      {/* Wrapper */}
       <div className="wrapper">
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/products">
+            <Route index element={<Product />} />
+          </Route>
         </Routes>
       </div>
     </div>
